3e3ce89357169f644f157bc574b3aed0eb56b077
abdel2000-dply <abno870@gmail.com>
2023-11-30 00:38:06 +0100

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,9 +8,14 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
-	if (!new || !parent)
+	if (!parent)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_t));
+
+	if (!new)
 		return (NULL);
 
 	new->n = value;
