1de3af9d09779a209cf234b3117ba3f2bd1ab49e
abdel2000-dply <abno870@gmail.com>
2023-12-01 00:39:27 +0100

    task 16 :fire: :fire:

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,15 +8,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh, rh;
+	size_t lh = 0, rh = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	lh = binary_tree_height(tree->left);
-	rh = binary_tree_height(tree->right);
+	if (tree->left)
+		lh = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		rh = binary_tree_height(tree->right) + 1;
 
-	return (1 + (lh > rh ? lh : rh));
+	return ((lh < rh) ? rh : lh);
 }
 
 
