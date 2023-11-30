91dfe734f3c97a6257a87fc7670e8795aaba0cb4
abdel2000-dply <abno870@gmail.com>
2023-12-01 00:21:37 +0100

    task 16 :fire: :fire:

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -35,25 +35,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree to measure the balance factor
- *
- * Return: Balance factor of the binary tree, 0 if tree is NULL
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int lh, rh;
-
-	if (tree == NULL)
-		return (0);
-
-	lh = (int)binary_tree_height(tree->left);
-	rh = (int)binary_tree_height(tree->right);
-
-	return (lh - rh);
-}
-
-/**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check
  *
